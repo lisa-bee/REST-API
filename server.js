e0649744aadcbae65ef822f8416d692b0808a30d
Lisa <lisa.bengtsson@live.com>
2020-04-08 13:58:45 +0200

    just a test initial setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+
+app.get("/", (req, res) => res.send("Welcome"))
+
+app.listen(port, () => console.log("Running example page"))
